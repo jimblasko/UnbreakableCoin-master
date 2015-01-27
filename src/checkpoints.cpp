a453c4eb0bd199a07f5a27f62a2b68c8ecc2bc3c
Melech <wardell.c@gmail.com>
2015-01-27 15:12:54 -0500

    Re-enabled checkpoints

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -70,7 +70,6 @@ namespace Checkpoints
     {
         if (!GetBoolArg("-checkpoints", true))
             return true;
-		return true;
 
         const MapCheckpoints& checkpoints = *Checkpoints().mapCheckpoints;
 
@@ -83,7 +82,6 @@ namespace Checkpoints
     double GuessVerificationProgress(CBlockIndex *pindex) {
         if (pindex==NULL)
             return 0.0;
-		return 0.0;
 
         int64 nNow = time(NULL);
 
@@ -113,7 +111,6 @@ namespace Checkpoints
 
     int GetTotalBlocksEstimate()
     {
-		return 0;
         if (!GetBoolArg("-checkpoints", true))
             return 0;
 
@@ -124,7 +121,6 @@ namespace Checkpoints
 
     CBlockIndex* GetLastCheckpoint(const std::map<uint256, CBlockIndex*>& mapBlockIndex)
     {
-		return NULL;
         if (!GetBoolArg("-checkpoints", true))
             return NULL;
 
